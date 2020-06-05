752bae568727d8d6db49892bfe721f860ffac596
lynx1986 <37790366+lynx1986@users.noreply.github.com>
2020-06-05 16:28:20 +0800

    Add files via upload

---
+++ precache-manifest.d8060faca2c869174b9b2dca8485c83c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce104e0cf6b429c7e86ce26d72443f8",
+    "url": "/react-element-typescript-admin/index.html"
+  },
+  {
+    "revision": "2cd5d99295e3b7738e0f",
+    "url": "/react-element-typescript-admin/static/css/2.2e801413.chunk.css"
+  },
+  {
+    "revision": "63898522a3a46682f05b",
+    "url": "/react-element-typescript-admin/static/css/main.4be420e9.chunk.css"
+  },
+  {
+    "revision": "2cd5d99295e3b7738e0f",
+    "url": "/react-element-typescript-admin/static/js/2.889227e3.chunk.js"
+  },
+  {
+    "revision": "8448b81c53401bac251b3188a99a9823",
+    "url": "/react-element-typescript-admin/static/js/2.889227e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63898522a3a46682f05b",
+    "url": "/react-element-typescript-admin/static/js/main.d3054b5e.chunk.js"
+  },
+  {
+    "revision": "640a5c24d4a7c5479ae6",
+    "url": "/react-element-typescript-admin/static/js/runtime-main.9bf40a23.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/react-element-typescript-admin/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/react-element-typescript-admin/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "0414741e5e6632c00889bd1d56047ce2",
+    "url": "/react-element-typescript-admin/static/media/iconfont.0414741e.woff"
+  },
+  {
+    "revision": "1ffc87fff3e6c3a1ffdcfe005d086352",
+    "url": "/react-element-typescript-admin/static/media/iconfont.1ffc87ff.ttf"
+  },
+  {
+    "revision": "d0c3b8c9a5864b10dc28da2f220b3bf7",
+    "url": "/react-element-typescript-admin/static/media/iconfont.d0c3b8c9.svg"
+  },
+  {
+    "revision": "e08cc87dc5c9fab1b76d1e6fccee1815",
+    "url": "/react-element-typescript-admin/static/media/iconfont.e08cc87d.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d18b7a2e1a0313da1ad1165584737f82.js"
+  "/react-element-typescript-admin/precache-manifest.d8060faca2c869174b9b2dca8485c83c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-element-typescript-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
