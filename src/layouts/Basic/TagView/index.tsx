79b346efddc87f7bbb36847636421bf5cb1660de
lynx86 <lynx86@126.com>
2020-06-07 22:25:57 +0800

    Add context menu

--- src/layouts/Basic/TagView/index.tsx
+++ src/layouts/Basic/TagView/index.tsx
@@ -3,11 +3,17 @@ import { withTranslation, WithTranslation } from 'react-i18next';
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Tag } from 'element-react';
 import { inject, observer } from 'mobx-react';
-import { AppState } from '../../../stores/app';
+import { Menu, Item, Separator, Submenu, MenuProvider } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
+
+import * as RouteUtil from '../../../utils/route';
+import app, { AppState } from '../../../stores/app';
 import { RouteItem } from '../../../routes';
 
 import styles from './index.module.scss';
+import './index.module.scss';
 import { UnregisterCallback } from 'history';
+
 
 export interface TagViewProps extends RouteComponentProps, WithTranslation {
     routes: RouteItem[];
@@ -63,17 +69,24 @@ class TagView extends React.Component<TagViewProps> {
 
         const { app, history, routes } = this.props;
 
-        const affixRoutes = filterAffixRoutes(routes);
-        const curRoute = findRoute(routes, history.location.pathname);
-        if (curRoute && (!curRoute.meta || !curRoute!.meta.affix)) {
-            affixRoutes.push(curRoute);
+        const initViews = filterAffixRoutes(routes);
+        const matchedRoutes = RouteUtil.getMatchedRoutes(history.location.pathname, routes);
+        if (matchedRoutes.length > 0) {
+            const matchedRoute = matchedRoutes[matchedRoutes.length - 1];
+            if (matchedRoute && (!matchedRoute.meta || !matchedRoute!.meta.affix)) {
+                initViews.push(matchedRoute);
+            }
         }
-        app!.initVisitedViews(affixRoutes);
+        app!.initVisitedViews(initViews);
 
         this.unlisten = history.listen(location => {
-            const curRoute = findRoute(this.props.routes, location.pathname);
-            if (curRoute) {
-                app?.addView(curRoute);
+
+            const matchedRoutes = RouteUtil.getMatchedRoutes(history.location.pathname, routes);
+            if (matchedRoutes.length > 0) {
+                const matchedRoute = matchedRoutes[matchedRoutes.length - 1];
+                if (matchedRoute.hidden) return;
+                matchedRoute.realPath = history.location.pathname;
+                app?.addView(matchedRoute);
             }
         });
     }
@@ -85,9 +98,36 @@ class TagView extends React.Component<TagViewProps> {
     render() {
 
         const views = this.props.app!.visitedViews;
+        
         return (
             <div className={styles.tagView}>
-                { views.map(view => this.renderTag(view)) }
+                { 
+                    views.map(view => 
+                        <MenuProvider id='context_menu' data={{ route: view }}>
+                            { this.renderTag(view) }
+                        </MenuProvider>
+                    ) 
+                }
+
+                <Menu id='context_menu' className={styles.contextMenu}>
+                    <Item data={{ command: 'refresh' }} onClick={this.handleContextMenu}>
+                        刷新
+                    </Item>
+                    <Item
+                     data={{ command: 'close' }} onClick={this.handleContextMenu}>
+                        关闭
+                    </Item>
+                    <Separator />
+                    <Item data={{ command: 'closeAll' }} onClick={this.handleContextMenu}>
+                        关闭所有
+                    </Item>
+                    <Item data={{ command: 'closeOthers' }} onClick={this.handleContextMenu}>
+                        关闭其它
+                    </Item>
+                    <Item data={{ command: 'closeRight' }} onClick={this.handleContextMenu}>
+                        关闭右侧
+                    </Item>
+                </Menu>
             </div>
         )
     }
@@ -95,13 +135,14 @@ class TagView extends React.Component<TagViewProps> {
     renderTag(route: RouteItem) {
 
         const { history, t } = this.props;
+        const pathname = history.location.pathname;
 
-        const isActive = route.fullPath === history.location.pathname;
+        const isActive = route.fullPath === pathname;
         console.log('path=' + route.path + ' fullPath=' + route.fullPath);
 
         if (route.meta && route.meta.affix) {
             return (
-                <Tag className={styles.tag} type={isActive ? 'success' : 'primary' } key={route.path}>
+                <Tag type={isActive ? 'success' : 'primary' } key={route.path}>
                     <span onClick={() => this.handleNavTo(route)}>
                         {t('route.' + route.name)}
                     </span>
@@ -129,7 +170,36 @@ class TagView extends React.Component<TagViewProps> {
     }
 
     handleNavTo = (route: RouteItem) => {
-        this.props.history.replace(route.fullPath!);
+        this.props.history.replace(route.realPath || route.fullPath!);
+    }
+
+    handleContextMenu = (data: any) => {
+        
+        const { history, app } = this.props;
+        const { command, route } = data.props;
+
+        switch (command) {
+
+        case 'refresh':
+            history.replace(history.location.pathname);
+            break;
+        case 'close':
+            app!.removeView(route); 
+            history.replace(app!.visitedViews[app!.visitedViews.length - 1].fullPath!);
+            break;
+        case 'closeAll':
+            app!.removeAllViews(); 
+            history.replace(app!.visitedViews[app!.visitedViews.length - 1].fullPath!);
+            break;
+        case 'closeOthers':
+            app!.removeOtherViews(route); 
+            history.replace(app!.visitedViews[app!.visitedViews.length - 1].fullPath!);
+            break;
+        case 'closeRight':
+            app!.removeViewsAfter(route); 
+            history.replace(app!.visitedViews[app!.visitedViews.length - 1].fullPath!);
+            break;
+        }
     }
 }
 
